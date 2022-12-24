04a0c90af3a4440936c8f09a9696f25cc77daa79
звук кайфа/diorblack <claudymen228@gmail.com>
2022-12-24 22:26:28 +0300

    resending activity requests

--- app/src/main/java/com/autonture/qrdocs/ui/generate_qr/GenerateQrCodeActivity.kt
+++ app/src/main/java/com/autonture/qrdocs/ui/generate_qr/GenerateQrCodeActivity.kt
@@ -26,6 +26,7 @@ import com.google.zxing.WriterException
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_generate_qr_code.*
 import kotlinx.android.synthetic.main.activity_generate_qr_code.view.*
+import org.jetbrains.anko.toast
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStream
@@ -33,14 +34,12 @@ import java.lang.Exception
 
 class GenerateQrCodeActivity : AppCompatActivity() {
 
-    private var imageUrl: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_qr_code)
 
         download_qr_btn.setOnClickListener {
-                saveImageToStorage()
+             //   saveImageToStorage()
         }
         create_qr_code_btn?.setOnClickListener {
             if (TextUtils.isEmpty(data_value.text.toString())) {
@@ -52,169 +51,13 @@ class GenerateQrCodeActivity : AppCompatActivity() {
             }
         }
     }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    downloadImage(imageUrl!!)
-                } else {
-
-
-                }
-                return
-            }
-            else -> {
-            }
-        }
-    }
-
-
-    private var msg: String? = ""
-    private var lastMsg = ""
-
-    @TargetApi(Build.VERSION_CODES.M)
-    fun askPermissions() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                )
-            ) {
-                AlertDialog.Builder(this, R.style.CustomAlertDialog)
-                    .setTitle("Permission required")
-                    .setMessage("Permission required to save photos from the Web.")
-                    .setPositiveButton("Allow") { dialog, id ->
-                        ActivityCompat.requestPermissions(
-                            this,
-                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
-                        )
-                        finish()
-                    }
-                    .setNegativeButton("Deny") { dialog, id -> dialog.cancel() }
-                    .show()
-            } else {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
-                )
-            }
-        } else {
-            //saveImageToStorage(imageUrl!!)
-        }
-    }
-
-
-
     private fun generateQrCode(text: String){
         val qrGenerator = QRGEncoder(text, null, QRGContents.Type.TEXT, 500)
         try {
             val bMap = qrGenerator.encodeAsBitmap()
             qr_image?.setImageBitmap(bMap)
-
-            imageUrl = text // convert to string
         } catch (e: WriterException){
-
+            Toast.makeText(this, "text exception", Toast.LENGTH_SHORT)
         }
-    }
-
-    fun saveImageToStorage(){
-        val externalStorage = Environment.getExternalStorageDirectory()
-        if (externalStorage.equals(Environment.MEDIA_MOUNTED)){
-            val storageDirectory = Environment.getExternalStorageDirectory().toString()
-            val file = File(storageDirectory,"test.jpg")
-            try {
-                val stream:OutputStream = FileOutputStream(file)
-                var drawable = ContextCompat.getDrawable(applicationContext, R.drawable.noresult)
-                var bitmap = (drawable as BitmapDrawable).bitmap
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-                stream.flush()
-                stream.close()
-                qr_image.setImageURI((Uri.parse(file.absolutePath)))
-                Toast.makeText(this, "Image saved successfully  ${Uri.parse((file.absolutePath))}",
-                Toast.LENGTH_SHORT).show()
-            }
-            catch (e: Exception){
-                e.printStackTrace()
-            }
-        }
-        else{
-
-        }
-    }
-
-    private fun statusMessage(url: String, directory: File, status: Int): String? {
-        var msg = ""
-        msg = when (status) {
-            DownloadManager.STATUS_FAILED -> "Download has been failed, please try again"
-            DownloadManager.STATUS_PAUSED -> "Paused"
-            DownloadManager.STATUS_PENDING -> "Pending"
-            DownloadManager.STATUS_RUNNING -> "Downloading..."
-            DownloadManager.STATUS_SUCCESSFUL -> "Image downloaded successfully in $directory" + File.separator + url.substring(
-                url.lastIndexOf("/") + 1
-            )
-            else -> "There's nothing to download"
-        }
-        return msg
-    }
-    @SuppressLint("Range")
-    private fun downloadImage(url: String) {
-        val directory = File(Environment.DIRECTORY_PICTURES)
-
-        if (!directory.exists()) {
-            directory.mkdirs()
-        }
-
-        val downloadManager = this.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-
-        val downloadUri = Uri.parse(url)
-
-        val request = DownloadManager.Request(downloadUri).apply {
-            setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-                .setAllowedOverRoaming(false)
-                .setTitle(url.substring(url.lastIndexOf("/") + 1))
-                .setDescription("")
-                .setDestinationInExternalPublicDir(
-                    directory.toString(),
-                    url.substring(url.lastIndexOf("/") + 1)
-                )
-        }
-
-        val downloadId = downloadManager.enqueue(request)
-        val query = DownloadManager.Query().setFilterById(downloadId)
-        Thread(Runnable {
-            var downloading = true
-            while (downloading) {
-                val cursor: Cursor = downloadManager.query(query)
-                cursor.moveToFirst()
-                if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
-                    downloading = false
-                }
-                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
-                msg = statusMessage(url, directory, status)
-                if (msg != lastMsg) {
-                    this.runOnUiThread {
-                        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-                    }
-                    lastMsg = msg ?: ""
-                }
-                cursor.close()
-            }
-        }).start()
-    }
-
-    companion object {
-        private const val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
     }
 }
--- app/src/main/java/com/autonture/qrdocs/ui/zoomfile/ZoomFileActivity.kt
+++ app/src/main/java/com/autonture/qrdocs/ui/zoomfile/ZoomFileActivity.kt
@@ -2,13 +2,16 @@ package com.autonture.qrdocs.ui.zoomfile
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.autonture.qrdocs.R
 import com.autonture.qrdocs.ui.Ultis
 import com.autonture.qrdocs.ui.link_storage.TemporaryLinkStorage
 import com.autonture.qrdocs.ui.scan_profile.ScanProfileActivity
 import kotlinx.android.synthetic.main.activity_zoom_file.*
+import java.lang.Exception
 
 class ZoomFileActivity: AppCompatActivity()  {
     lateinit var imageView: ImageView
@@ -16,16 +19,35 @@ class ZoomFileActivity: AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zoom_file)
-        val bundle = intent.extras
-        if (bundle != null){
-            url = bundle.getString("link")
+        val handler = Handler()
+        val runnable = object : Runnable {
+            override fun run() {
+                try {
+                    recreate()
+                } catch (e: Exception) {}
+                handler.postDelayed(this, 50000)
+            }
         }
-        imageView = findViewById(R.id.image_result)
-        Ultis().fetchSVG(this,url!!,imageView)
-        exit_zoom_btn.setOnClickListener {
-            startActivity(Intent(this, ScanProfileActivity::class.java))
+        handler.postDelayed(runnable, 50000)
+
+        try {
+            val bundle = intent.extras
+            if (bundle != null){
+                url = bundle.getString("link")
+            }
+            imageView = findViewById(R.id.image_result)
+            Ultis().fetchSVG(this,url!!,imageView)
+            exit_zoom_btn.setOnClickListener {
+                startActivity(Intent(this, ScanProfileActivity::class.java))
+            }
+
         }
+        catch (e:Exception){
+            Toast.makeText(this, "update", Toast.LENGTH_LONG).show()
+        }
+
     }
+
 }
 
 //"https://www.svgrepo.com/show/197208/waiting-room.svg" test link
